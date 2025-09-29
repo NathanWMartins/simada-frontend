384211ab6b6a2a1e3b9f561dd44abbcd080103c9
NathanWMartins <nathanwillmartins@gmail.com>
2025-09-29 17:07:01 -0300

    fix: Correção da variavel para CRA

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "../contexts/UserContext";
 
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 console.log("API BASE", apiBaseUrl);
 export const api = axios.create({ baseURL: apiBaseUrl });
 
